9eb71fcea8950dda00671d7442fc3116c4490a89
joao66br <joao66br@yahoo.com.br>
2016-05-08 17:59:07 -0300

    inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>Ventania Veleiros</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="dist/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="img/marca_hor.png" alt="logo" height="150">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        Oi, bem-vindo à Ventania! Somos uma empresa de aluguel de barcos em Búzios que gostaria de
+                        convidá-lo a sentir a emoção de velejar nessa cidade paradisíaca e de ótimos ventos.
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="dist/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="dist/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
