ad9ddf54d53cb2009537f32a2cdbc88f3491ae7d
Joao Penna Andrade <joao66br@yahoo.com.br>
2016-06-03 22:19:15 -0300

    avancando na página inicial

--- index.php
+++ index.php
@@ -10,18 +10,86 @@
     <body>
         <div class="container-fluid">
             <div class="row">
+                <div class="col-md-8">
+                    <img src="img/marca_hor.png" alt="logo" class="img-responsive" style="max-height: 150px" >
+                </div>
+            </div>
+            <div class="row">
+                <img src="img/header_wordpress.JPG" class="img-responsive">
                 <div class="col-md-12">
-                    <img src="img/marca_hor.png" alt="logo" height="150">
+                    <h1>Bem-vindo</h1>
+                    <h4>
+                        Oi e bem-vindo à Ventania! Somos uma empresa de aluguel de barcos a vela em Búzios e gostaríamos de
+                        convidá-lo a sentir a emoção de velejar nessa cidade paradisíaca e de ótimos ventos.
+                    </h4>
+                    
+                    <h4>
+                        Se você já veleja, não perca tempo e pegue o leme de um de nossos barcos para um bordo inesquecível. Se ainda não, 
+                        venha fazer uma aula.
+                    </h4>
+                    <img src="img/icone.png" style="height: 40px" class="img-responsive center-block">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <p>
-                        Oi, bem-vindo à Ventania! Somos uma empresa de aluguel de barcos em Búzios que gostaria de
-                        convidá-lo a sentir a emoção de velejar nessa cidade paradisíaca e de ótimos ventos.
-                    </p>
+                    <h1>Os barcos</h1>
+                    <h4>
+                        (As seguintes fotos são de barcos do mesmo modelo, não dos nossos barcos)
+                    </h4>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Laser</h4>
+                    <img src="img/laser.jpg" class="img-responsive" style="max-height: 300px">
+                </div>
+                <div class="col-md-4">
+                    <h4>Hobie Wave</h4>
+                    <img src="img/wave_nav.jpg" class="img-responsive" style="max-height: 300px">
+                </div>
+                <div class="col-md-4">
+                    <h4>Hobie 16</h4>
+                    <img src="img/h16.jpg" class="img-responsive" style="max-height: 300px">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <br>
+                    <h4>
+                        Temos um Laser, um Hobie Wave e um Hobie 16, todos em boas condições. Clique na imagem para saber mais sobre os barcos.
+                    </h4>
+                    <img src="img/icone.png" style="height: 40px" class="img-responsive center-block">
+                    <h1>Segurança</h1>
+                    <h4>
+                        Navegar em um barco a vela no mar envolve certos riscos em vista dos quais tomamos precauções como:
+                        <ul>
+                            <li>Fornecer e cobrar o uso de coletes salva-vidas</li>
+                            <li>Solicitar a declaração de experiência por escrito</li>
+                            <li>Equipar cada barco com um telefone celular para pedir socorro</li>
+                            <li>Dispor de bote inflável motorizado para resgate, binóculo e rádio VHF</li>
+                            <li>Fazer um briefing de segurança</li>
+                        </ul>
+                    </h4>
+                    <img src="img/icone.png" style="height: 40px" class="img-responsive center-block">
+                    <h1>Comodidade</h1>
+                    <h4>
+                        Oferecemos como cortesia protetor solar e chapéu, para que o passeio não tenha como consequencia desagradáveis queimaduras.
+                        Para não correr o risco de o barco desejado estar indisponível, oferecemos a possibilidade de reservar on-line.
+                    </h4>
+                    <img src="img/icone.png" style="height: 40px" class="img-responsive center-block">
+                    <h1>Opcionais</h1>
+                    <h4>
+                        Para que você tenha uma boa recordação do velejo, oferemos a gravação com uma câmera GoPro. E para deixar deixar seu passeio mais divertido,
+                        uma caixa de som bluetooth a prova d'água.
+                    </h4>
+                    <img src="img/icone.png" style="height: 40px" class="img-responsive center-block">
+                    <h1>Preços</h1>
+                    <h4>
+                        colocar info sobre preços
+                    </h4>
+                </div>
+            </div>
+            
             
         </div>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
